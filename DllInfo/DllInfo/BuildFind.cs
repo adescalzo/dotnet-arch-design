ed8415cc48121ba706cc5634cf632f4d5405c6a9
andres descalzo <andresdescalzo@gmail.com>
2019-02-22 07:44:40 -0300

    add all

---
+++ DllInfo/DllInfo/BuildFind.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace DllInfo
+{
+    internal class BuildFind
+    {
+        internal static int GetBuildType(string AssemblyName)
+        {
+            Assembly assm = Assembly.LoadFrom(AssemblyName);
+
+            object[] attributes = assm.GetCustomAttributes(typeof(DebuggableAttribute), false);
+
+            if (attributes.Length == 0)
+            {
+                Console.WriteLine($"{AssemblyName} is a RELEASE Build....");
+
+                return 0;
+            }
+
+            foreach (Attribute attr in attributes)
+            {
+                if (attr is DebuggableAttribute)
+                {
+                    DebuggableAttribute d = attr as DebuggableAttribute;
+
+                    Console.WriteLine($"Run time Optimizer is enabled : {!d.IsJITOptimizerDisabled}");
+
+                    Console.WriteLine($"Run time Tracking is enabled : {d.IsJITTrackingEnabled}");
+
+                    if (d.IsJITOptimizerDisabled == true)
+                    {
+                        Console.WriteLine(String.Format("{0} is a DEBUG Build....", AssemblyName));
+
+                        return 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine(String.Format("{0} is a RELEASE Build....", AssemblyName));
+
+                        return 0;
+                    }
+                }
+            }
+
+            return 3;     
+        }
+    }
+}
---
+++ DllInfo/DllInfo/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace DllInfo
+{
+    class Program
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage GetBuildType <assemblyName>");
+
+                return 2;
+            }
+
+            return BuildFind.GetBuildType(args[0]);
+        }
+
+        static int CheckEnviroment(string[] args)
+        { 
+            string inputPath = string.Empty;
+
+            if (args.Length == 1 && Directory.Exists(args[0]))
+            {
+                inputPath = args[0];
+            }
+            else
+            {
+                Console.WriteLine("Input path does not exist. Check input arguments.");
+
+                return 2;
+            }
+
+            string[] searchExtensions = { ".dll", ".exe", ".lib" };
+
+            var files = Directory
+                        .GetFiles(inputPath, "*.*", SearchOption.TopDirectoryOnly)
+                        .Where(file => searchExtensions.Any(ext => file.ToLower().Contains(ext)))
+                        .ToList();
+
+            uint releaseCount = 0;
+
+            uint debugCount = 0;
+
+            StreamWriter sw = new StreamWriter("output.txt", false);
+
+            foreach (var file in files)
+            {
+                FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(file);
+
+                sw.Write(file);
+                sw.Write("\t\t");
+
+                if (fileVersionInfo.IsDebug)
+                {
+                    sw.Write("Debug");
+                    debugCount++;
+                }
+                else
+                {
+                    sw.Write("Release");
+                    releaseCount++;
+                }
+
+                sw.Write(Environment.NewLine);
+            }
+
+            sw.Write(Environment.NewLine);
+            sw.WriteLine("Debug: " + debugCount.ToString());
+            sw.WriteLine("Release: " + releaseCount.ToString());
+            sw.Flush();
+            sw.Close();
+            sw.Dispose();
+
+            Console.WriteLine("Output successfully written to: " + new FileInfo("output.txt").FullName);
+
+            return 0;
+        }
+    }
+}
